16a3e1b88d0745a30af962cd62988b84ce405fa4
Alan <alaneduardocs42@gmail.com>
2025-09-10 19:37:53 -0600

    Instalación Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,21 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
+ imports: [DatePipe],
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'basesAngular';
+  title = 'Alan Sánchez';
+  duplicarNumero(valor: number): number {
+    return valor * 2;
+  }
+
+  peliculas = {
+    titulo: 'El Padrino',
+    fechaLanzamiento: new Date(1972, 2, 24),
+    director: 'Francis Ford Coppola'
+  };
 }
+import { DatePipe } from '@angular/common';
